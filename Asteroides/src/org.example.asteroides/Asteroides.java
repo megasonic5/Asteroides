64ec0492d06b35d61a1b8e0af1368914a7c4e71e
megasonic5 <168662773+megasonic5@users.noreply.github.com>
2025-02-28 11:30:52 +0100

    Update Asteroides.java

--- Asteroides/src/org.example.asteroides/Asteroides.java
+++ Asteroides/src/org.example.asteroides/Asteroides.java
@@ -1,17 +1,20 @@
 package org.example.asteroides;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Asteroides extends Activity {
     private Button bAcercaDe;
 
-    @Override public void onCreate(Bundle savedInstanceState) {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         bAcercaDe = (Button) findViewById(R.id.button03);
-        bAcercaDe.setOnClickListener(new OnClickListener() {
+        bAcercaDe.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 lanzarAcercaDe(null);
             }
