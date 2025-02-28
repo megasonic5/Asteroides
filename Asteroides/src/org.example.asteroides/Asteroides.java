16b4993b81dc0a4490443512f67976f3f81e9c5e
megasonic5 <168662773+megasonic5@users.noreply.github.com>
2025-02-28 11:26:51 +0100

    Update Asteroides.java

--- Asteroides/src/org.example.asteroides/Asteroides.java
+++ Asteroides/src/org.example.asteroides/Asteroides.java
@@ -1,1 +1,25 @@
+package org.example.asteroides;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Asteroides extends Activity {
+    private Button bAcercaDe;
+
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        bAcercaDe = (Button) findViewById(R.id.button03);
+        bAcercaDe.setOnClickListener(new OnClickListener() {
+            public void onClick(View view) {
+                lanzarAcercaDe(null);
+            }
+        });
+    }
+
+    public void lanzarAcercaDe(View view) {
+        Intent i = new Intent(this, AcercaDe.class);
+        startActivity(i);
+    }
+}
