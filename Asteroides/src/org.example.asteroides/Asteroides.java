5c96976bdf9f8af5936e04c3eee307a8a048a90c
megasonic5 <168662773+megasonic5@users.noreply.github.com>
2025-03-02 17:12:32 +0000

    salir

--- Asteroides/src/org.example.asteroides/Asteroides.java
+++ Asteroides/src/org.example.asteroides/Asteroides.java
@@ -4,19 +4,27 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class Asteroides extends Activity {
     private Button bAcercaDe;
+    private Button bSalir;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        bAcercaDe = (Button) findViewById(R.id.button03);
+        bAcercaDe = (Button) findViewById(R.id.Button03);
         bAcercaDe.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 lanzarAcercaDe(null);
+            }
+        });
+        bSalir = (Button) findViewById(R.id.Button04);
+        bSalir.setOnClickListener(new OnClickListener() {
+            public void onClick(View view) {
+                finish();
             }
         });
     }
