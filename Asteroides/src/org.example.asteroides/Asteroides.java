86f3c9b9b3ff739dc3046b1b17549a1dca7e44c8
megasonic5 <168662773+megasonic5@users.noreply.github.com>
2025-03-03 10:21:04 +0000

    Configuracion.

--- Asteroides/src/org.example.asteroides/Asteroides.java
+++ Asteroides/src/org.example.asteroides/Asteroides.java
@@ -10,11 +10,18 @@ import android.widget.Button;
 public class Asteroides extends Activity {
     private Button bAcercaDe;
     private Button bSalir;
+    private Button bConfigurar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        bConfigurar = (Button) findViewById(R.id.Button02);
+        bConfigurar.setOnClickListener(new OnClickListener() {
+            public void onClick(View view) {
+                mostrarPreferencias(null);
+            }
+        });
         bAcercaDe = (Button) findViewById(R.id.Button03);
         bAcercaDe.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -27,6 +34,11 @@ public class Asteroides extends Activity {
                 finish();
             }
         });
+    }
+
+    public void mostrarPreferencias(View view) {
+        Intent i = new Intent(this, Preferencias.class);
+        startActivity(i);
     }
 
     public void lanzarAcercaDe(View view) {
